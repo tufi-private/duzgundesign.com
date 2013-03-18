27bc686af244be5e62f09f99b23d2db2b7f34a03
tufi-private <k7S5A*79>
2013-03-18 23:02:15 +0100

    Düzgün Design

---
+++ public/index.php
@@ -0,0 +1,177 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Düzgün Design</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/reset.css" type="text/css" media="screen">
+    <link rel="stylesheet" href="css/style.css" type="text/css" media="screen">
+    <link rel="stylesheet" href="css/grid24.css" type="text/css" media="screen">
+    <script src="js/jquery-1.6.4.min.js" type="text/javascript"></script>
+    <script src="js/ff_cash.js" type="text/javascript"></script>
+    <script src="js/superfish.js" type="text/javascript"></script>
+    <script src="js/jquery.prettyPhoto.js" type="text/javascript"></script>
+    <script src="js/tms-0.3.2.js" type="text/javascript"></script>
+    <script src="js/tms_presets.js" type="text/javascript"></script>
+    <!--[if lt IE 7]>
+    <div style='clear:both; text-align:center; position:relative;'><a
+        href="http://windows.microsoft.com/en-US/internet-explorer/products/ie/home?ocid=ie6_countdown_bannercode"><img
+        src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg"
+        border="0" height="42" width="820"
+        alt="Eski bir tarayıcı kullanıyorsunuz.Daha hızlı ve güvenli internet kullanımı için, tarayıcınızı hemen şimdi yenileyin."/></a>
+    </div>
+    <![endif]-->
+    <!--[if lt IE 9]>
+    <script type="text/javascript" src="js/html5.js"></script>
+    <link rel="stylesheet" href="css/ie.css" type="text/css" media="screen">
+    <![endif]-->
+</head>
+<body id="page1">
+<div class="block">
+<!--==============================header=================================-->
+<header class="header">
+    <div class="row-1">
+        <div class="main">
+            <h1><a href="index.php">Morcan</a></h1>
+            <nav>
+                <ul class="sf-menu">
+                    <li><a class="current" href="index.php">ANA SAYFA</a></li>
+                    <li><a href="#">HAKKIMIZDA</a></li>
+                    <li><a href="#">ÜRÜNLERİMİZ</a>
+                        <ul>
+                            <li>
+                                <div class="wrapper">
+                                    <figure class="menu-img">
+                                        <img src="images/menu-img1.jpg" width="61" height="51" alt="">
+                                    </figure>
+                                    <div class="extra-wrap">
+                                        <a href="#">Mutfak Dolapları</a>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="item">
+                                <div class="wrapper">
+                                    <figure class="menu-img">
+                                        <img src="images/menu-img2.jpg" width="61" height="51" alt="">
+                                    </figure>
+                                    <div class="extra-wrap">
+                                        <a href="#">Banyo Dolapları</a>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="wrapper">
+                                    <figure class="menu-img">
+                                        <img src="images/menu-img3.jpg" width="61" height="51" alt="">
+                                    </figure>
+                                    <div class="extra-wrap">
+                                        <a href="#">Gardroplar</a>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="item">
+                                <div class="wrapper">
+                                    <figure class="menu-img">
+                                        <img src="images/menu-img4.jpg" width="61" height="51" alt="">
+                                    </figure>
+                                    <div class="extra-wrap">
+                                        <a href="#">Şifonyerler</a>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="wrapper">
+                                    <figure class="menu-img">
+                                        <img src="images/menu-img3.jpg" width="61" height="51" alt="">
+                                    </figure>
+                                    <div class="extra-wrap">
+                                        <a href="#">TV Üniteleri</a>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="item">
+                                <div class="wrapper">
+                                    <figure class="menu-img">
+                                        <img src="images/menu-img4.jpg" width="61" height="51" alt="">
+                                    </figure>
+                                    <div class="extra-wrap">
+                                        <a href="#">Makyaj Masaları</a>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="wrapper">
+                                    <figure class="menu-img">
+                                        <img src="images/menu-img3.jpg" width="61" height="51" alt="">
+                                    </figure>
+                                    <div class="extra-wrap">
+                                        <a href="#">Kitaplık Modelleri</a>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="item">
+                                <div class="wrapper">
+                                    <figure class="menu-img">
+                                        <img src="images/menu-img4.jpg" width="61" height="51" alt="">
+                                    </figure>
+                                    <div class="extra-wrap">
+                                        <a href="#">Kapı Modelleri</a>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </li>
+                    <li><a href="#">İLETİŞİM</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+    <div class="row-2">
+        <div class="main">
+            <div class="slider">
+                <ul class="items">
+                    <li><img src="images/page1-img1-big.jpg" alt=""></li>
+                    <li><img src="images/page1-img2-big.jpg" alt=""></li>
+                    <li><img src="images/page1-img3-big.jpg" alt=""></li>
+                    <li><img src="images/page1-img4-big.jpg" alt=""></li>
+                    <li><img src="images/page1-img5-big.jpg" alt=""></li>
+                    <li><img src="images/page1-img6-big.jpg" alt=""></li>
+                </ul>
+                <ul class="pagination">
+                    <li><a class="item1" href="#"></a><span></span></li>
+                    <li><a class="item2" href="#"></a><span></span></li>
+                    <li><a class="item3" href="#"></a><span></span></li>
+                    <li><a class="item4" href="#"></a><span></span></li>
+                    <li><a class="item5" href="#"></a><span></span></li>
+                    <li class="last-item"><a class="item6" href="#"></a><span></span></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</header>
+<!--==============================End header=================================-->
+
+<!--==============================content================================-->
+<!--// content part-->
+<!--==============================End content================================-->
+
+<!--==============================footer=================================-->
+<footer>
+    <div class="main">
+        <div class="wrapper">
+            <ul class="menu-bot">
+                <li><a class="active" href="index.html">Ana Sayfa</a></li>
+                <li><a href="#">Hakkımızda</a></li>
+                <li><a href="#">Ürünlerimiz</a></li>
+                <li><a href="#">İletişim</a></li>
+            </ul>
+            <div class="text-bot">
+                Düzgün Design &copy; 2013
+            </div>
+        </div>
+        <div class="footer-link"><!-- {%FOOTER_LINK} --></div>
+    </div>
+</footer>
+<!--==============================End footer=================================-->
+</div>
+</body>
+</html>
