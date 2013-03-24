89cc76c005db2a0f5dda332c3c35c7c345498fa7
tufi-private <k7S5A*79>
2013-03-24 23:02:53 +0100

    Düzgün Design

--- public/index.php
+++ public/index.php
@@ -1,3 +1,11 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+if (isset($_GET['d'])) {
+    include './tpl/'.$_GET['d'].'.phtml';
+    exit;
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
